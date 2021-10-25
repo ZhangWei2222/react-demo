3099871a49a82700f1a10ea295eca5c9a7ba214f
zhangwei66 <zhangwei66@xiaomi.com>
2021-10-25 16:50:02 +0800

    feat: 事件冒泡

--- src/index.js
+++ src/index.js
@@ -3,18 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: 'zw'
-    }
-  }
-
   render() {
-    return (<div>{this.state.name}</div>)
+    return (
+      <div
+        onClick={() => {
+          console.log("rooter click");
+        }}
+      >
+        <p>我想出现在root中</p>
+        <Test />
+      </div>
+    )
   }
 }
 
+function Test() {
+  return ReactDOM.createPortal(
+    <ChildA />,
+    //     <h1>我想出现在container中</h1>,
+    document.getElementById("container")
+  );
+}
+
+function ChildA() {
+  return <p>我是childA</p>;
+}
 
 ReactDOM.render(
   <App />,
